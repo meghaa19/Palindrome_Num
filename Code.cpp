5669516bdd285629248abf0c9de28c4f0e5a7edc
Megha <92908549+meghaa19@users.noreply.github.com>
2022-10-16 12:41:19 +0530

    Create Code.cpp

---
+++ Code.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+        string str_num=to_string(x);
+        string str_num_rev=str_num;
+        reverse(str_num_rev.begin(),str_num_rev.end());
+        if(str_num==str_num_rev)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+        
+        
+    }
+};
